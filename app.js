fa61bf173d10453c59b76f54cf2546a8b0d916f4
JonelSV <svjonel@gmail.com>
2021-08-23 08:15:42 -0400

    video and js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
     item.onclick = () => {
         item.parentElement.parentElement.classList.
-        toggle()
+        toggle("change")
     }
 })
