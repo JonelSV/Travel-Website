4eb0059919cf9661b2e0f64fb8c33ac62aa27eea
JonelSV <svjonel@gmail.com>
2021-08-23 18:56:14 -0400

    navbar done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const container = document.querySelector(".container")
 
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+    container.classList.add("change");
+})
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+    container.classList.remove("change");
+})
+
 const colors = ["#6495ed", "#7fffd4","#ffa07a","#f08080", "#afeeee"];
 
 let i = 0;
