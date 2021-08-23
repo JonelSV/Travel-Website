907f9423cc41de494426fc97e2835a79c62f9fb4
JonelSV <svjonel@gmail.com>
2021-08-23 18:23:02 -0400

    js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const colors = ["#6495ed", "#7fffd4","#ffa07a","#f08080", "#afeeee"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach((item) =>{
+    item.style.cssText = `background-color: ${colors[i++]}`
+})
+
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
     item.onclick = () => {
         item.parentElement.parentElement.classList.
